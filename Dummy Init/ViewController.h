76809a1ae20f10c429f8207456f6873c30c9badc
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-05-21 15:26:12 -0500

    Initial Commit

---
+++ Dummy Init/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dummy Init
+//
+//  Created by Walter Gonzalez Domenzain on 21/05/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
